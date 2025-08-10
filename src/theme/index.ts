860b3715771ba525e5f735ba890a04158db74802
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-08-10 10:35:39 -0300

    Feat: Added new theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,29 +1,32 @@
 export default {
   COLORS: {
-    WHITE: '#FFFFFF',
-
-    GREEN_700: '#00875F',
-    GREEN_500: '#00B37E',
-
-    RED: '#F75A68',
-    RED_DARK: '#AA2834',
-
-    GRAY_700: '#121214',
-    GRAY_600: '#202024',
-    GRAY_500: '#29292E',
-    GRAY_400: '#323238',
-    GRAY_300: '#7C7C8A',
-    GRAY_200: '#C4C4CC',
-    GRAY_100: '#E1E1E6'
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+    GRAY_1: '#1B1D1E',
+    GRAY_2: '#333638',
+    GRAY_3: '#5C6265',
+    GRAY_4: '#B9BBBC',
+    GRAY_5: '#DDDEDF',
+    GRAY_6: '#EFF0F0',
+    GRAY_7: '#FAFAFA',
+    WHITE: '#FFFFFF'
   },
+
   FONT_FAMILY: {
-    REGULAR: 'Roboto_400Regular',
-    BOLD: 'Roboto_700Bold'
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
   },
+
   FONT_SIZE: {
-    SM: 14,
-    MD: 16,
-    LG: 18,
-    XL: 24
+    SM: 12,
+    MD: 14,
+    LG: 16,
+    XL: 18,
+    XXL: 24,
+    XXXL: 32
   }
 };
