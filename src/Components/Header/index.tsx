6ef303d517f5879329af8244aaf1f4d40ed0cf13
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-13 23:06:55 -0300

    Fix: Remove navigation

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,12 +3,6 @@ import { Container, Logo } from "./styles";
 import logoImg from '@/assets/LogoIcon/Logo.png';
 
 export function Header(){
-
-    const navigation = useNavigation();
-
-    function handleGoBack(){
-    navigation.navigate('groups')
-    }
     return(
         <Container>
             <Logo source={logoImg}/>
