f34a27baa813cf09105557cd791c20cd988ef33a
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-13 23:07:29 -0300

    Fix: Delete unused archive

--- src/screens/Groups/styles.tsx
+++
@@ -1,7 +0,0 @@
-import theme from "@/src/theme";
-import styled from "styled-components/native";
-
-export const Background = styled.SafeAreaView`
-    flex: 1;
-    background-color: ${theme.COLORS.GRAY_7};
-`;
