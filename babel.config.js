0014ab550e44ad7298d9d10bdbeb5a6f07d30ff7
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-15 01:16:41 -0300

    Feat: Added mew config

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            '@img': './src/img',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@storage': './src/storage',
-            '@utils': './src/utils',
-          },
-        },
-      ],
-    ],
   };
 };
