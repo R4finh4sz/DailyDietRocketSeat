e71a41b5179d626582bcdd4687b872579de65c39
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-09-20 14:11:05 -0300

    Styles: Added margin-bottom

--- src/App/Home/styles.tsx
+++ src/App/Home/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: #FAFAFA;
+  margin-bottom: 30px;
 `;
 
 export const RefeicoesTile = styled.Text`
