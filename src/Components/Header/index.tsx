3d897f5908a016eeacedd23ea74e8b5ea9cf2d45
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-09-20 13:39:32 -0300

    Feat: Added Profile

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { useNavigation } from "@react-navigation/native";
-import { Container, Logo } from "./styles";
+import { Container, Logo, ProfileStyle } from "./styles";
 import logoImg from '@/assets/LogoIcon/Logo.png';
+import Profile from "@/assets/Profile/Profile.png";
+import { Touchable, TouchableOpacity } from "react-native";
+
 
 export function Header(){
     return(
         <Container>
             <Logo source={logoImg}/>
+            <TouchableOpacity><ProfileStyle source={Profile}/></TouchableOpacity>
+            
         </Container>
     )
 }
--- src/Components/Header/styles.tsx
+++ src/Components/Header/styles.tsx
@@ -4,10 +4,19 @@ export const Container = styled.View`
     width: 100%;
     flex-direction: row;
     align-items: flex-start;
-    justify-content: flex-start;
+    justify-content: space-between;
+    flex-direction: row;
+    padding: 24px;
+    margin-top: 10px;
 `;
 
 export const Logo = styled.Image`
   width: 82px;
   height: 37px;
+  margin-top: 10px;
+`;
+
+export const ProfileStyle = styled.Image`
+  width: 50px;
+  height: 50px;
 `;
