c9111d3f021f0925f7680219fba622932b2d45a6
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-09-20 13:39:41 -0300

    Feat: Added theme

--- src/Components/PercentCard/styles.tsx
+++ src/Components/PercentCard/styles.tsx
@@ -1,3 +1,4 @@
+import theme from '@/src/theme';
 import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
@@ -17,13 +18,14 @@ export const ArrowIcon = styled.View`
 
 export const Percentage = styled.Text`
   font-size: 32px;
-  font-weight: bold;
+  font-family: ${theme.FONT_FAMILY.BOLD};
   color: #1B1D1E;
   margin-bottom: 2px;
 `;
 
 export const Description = styled.Text`
   font-size: 14px;
-  color: #5C6265;
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  color: '#333638';
   text-align: center;
 `;
