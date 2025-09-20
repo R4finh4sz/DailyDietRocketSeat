986505d3375df83e8f70376e1af60fd6e70bebba
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-09-20 14:10:52 -0300

    Feat: Added import mock and moved scrollview

--- src/App/Home/index.tsx
+++ src/App/Home/index.tsx
@@ -6,18 +6,7 @@ import { PercentCard } from '@/src/Components/PercentCard';
 import { NewMealButton } from '@/src/Components/NewMealButton';
 import { MealSection } from '@/src/Components/MealSection';
 import { useRouter } from 'expo-router';
-
-const mockData = [
-  {
-    date: '12.08.22',
-    meals: [
-      { id: '1', time: '20:00', name: 'X-tudo', status: 'outside' as const },
-      { id: '2', time: '16:00', name: 'Whey protein com leite', status: 'within' as const },
-      { id: '3', time: '12:30', name: 'Salada cesar com frango...', status: 'within' as const },
-      { id: '4', time: '09:30', name: 'Vitamina de banana com...', status: 'within' as const }
-    ]
-  },
-];
+import { mockData } from '@/Mocks/MockDataListaComida';
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -31,6 +20,7 @@ export default function HomeScreen() {
   };
 
   return (
+  <ScrollView showsVerticalScrollIndicator={false} bounces={false} overScrollMode='never' style={{ flex: 1, backgroundColor: '#FAFAFA' }}>
     <Container>
       <Header />
       <PercentCard 
@@ -41,7 +31,7 @@ export default function HomeScreen() {
       <RefeicoesTile>Refeições</RefeicoesTile>
       <NewMealButton onPress={handleNewMealPress} />
       
-      <ScrollView showsVerticalScrollIndicator={false}>
+
         {mockData.map((section, index) => (
           <MealSection 
             key={`${section.date}-${index}`}
@@ -49,7 +39,8 @@ export default function HomeScreen() {
             meals={section.meals}
           />
         ))}
-      </ScrollView>
+  
     </Container>
+        </ScrollView>
   );
 }
