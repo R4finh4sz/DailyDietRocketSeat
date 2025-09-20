789069a7017d3deaaaf75df6245c06f925d9c9c2
Rafael Souza Santana <rafael.souza6657@gmail.com>
2025-09-20 14:10:35 -0300

    Feat: Added new mock

---
+++ Mocks/MockDataListaComida.ts
@@ -0,0 +1,29 @@
+export const mockData = [
+  {
+    date: '14.08.22',
+    meals: [
+      { id: '1', time: '20:00', name: 'X-tudo', status: 'outside' as const },
+      { id: '2', time: '16:00', name: 'Whey protein com leite', status: 'within' as const },
+      { id: '3', time: '12:30', name: 'Salada cesar com frango...', status: 'within' as const },
+      { id: '4', time: '09:30', name: 'Vitamina de banana com...', status: 'within' as const }
+    ]
+  },
+  {
+    date: '13.08.22',
+    meals: [
+      { id: '1', time: '20:00', name: 'X-tudo', status: 'outside' as const },
+      { id: '2', time: '16:00', name: 'Whey protein com leite', status: 'within' as const },
+      { id: '3', time: '12:30', name: 'Salada cesar com frango...', status: 'within' as const },
+      { id: '4', time: '09:30', name: 'Vitamina de banana com...', status: 'within' as const }
+    ]
+  },
+  {
+    date: '12.08.22',
+    meals: [
+      { id: '1', time: '20:00', name: 'X-tudo', status: 'outside' as const },
+      { id: '2', time: '16:00', name: 'Whey protein com leite', status: 'within' as const },
+      { id: '3', time: '12:30', name: 'Salada cesar com frango...', status: 'within' as const },
+      { id: '4', time: '09:30', name: 'Vitamina de banana com...', status: 'within' as const }
+    ]
+  },
+];
