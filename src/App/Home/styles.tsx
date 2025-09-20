32215a4f6ce77b30d5243920ada9fe16278e4ffc
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-20 16:59:53 -0300

    Feat: Added SafeAreaView

--- src/App/Home/styles.tsx
+++ src/App/Home/styles.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
-  background-color: #FAFAFA;
+  background-color: #fafafa;
   margin-bottom: 30px;
 `;
 
 export const RefeicoesTile = styled.Text`
-  color: #1B1D1E;
+  color: #1b1d1e;
   font-size: 16px;
   font-weight: normal;
   margin: 0 24px 8px 24px;
