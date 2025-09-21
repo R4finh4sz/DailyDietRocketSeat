dd30a2ac657b6312e8becb7ffba214ec93d4ca16
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-20 22:06:58 -0300

    Update index.tsx

--- src/App/Home/index.tsx
+++ src/App/Home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
-import { Container, RefeicoesTile } from './styles';
-import { Header } from '@/src/Components/Header';
-import { PercentCard } from '@/src/Components/PercentCard';
-import { NewMealButton } from '@/src/Components/NewMealButton';
-import { MealSection } from '@/src/Components/MealSection';
-import { useRouter } from 'expo-router';
-import { mockData } from '@/Mocks/MockDataListaComida';
+import {ScrollView, View} from 'react-native';
+import {Container, RefeicoesTile} from './styles';
+import {Header} from '@/src/Components/Header';
+import {PercentCard} from '@/src/Components/PercentCard';
+import {Button} from '@/src/Components/Button';
+import {MealSection} from '@/src/Components/MealSection';
+import {useRouter} from 'expo-router';
+import {mockData} from '@/Mocks/MockDataListaComida';
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -20,27 +20,36 @@ export default function HomeScreen() {
   };
 
   return (
-  <ScrollView showsVerticalScrollIndicator={false} bounces={false} overScrollMode='never' style={{ flex: 1, backgroundColor: '#FAFAFA' }}>
-    <Container>
-      <Header />
-      <PercentCard 
-        percentage="90,86%" 
-        onPress={handlePercentCardPress}
-      />
-      
-      <RefeicoesTile>Refeições</RefeicoesTile>
-      <NewMealButton onPress={handleNewMealPress} />
-      
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      bounces={false}
+      overScrollMode="never"
+      style={{flex: 1, backgroundColor: '#FAFAFA'}}>
+      <Container>
+        <Header />
+        <PercentCard percentage="90,86%" onPress={handlePercentCardPress} />
+
+        <RefeicoesTile>Refeições</RefeicoesTile>
+        <View style={{paddingHorizontal: 24}}>
+          <Button
+            onPress={() => router.push('/NewMeal')}
+            backgroundColor="#1B1D1E"
+            textColor="#FAFAFA"
+            iconName="plus"
+            width="100%"
+            iconSize={18}>
+            Nova refeição
+          </Button>
+        </View>
 
         {mockData.map((section, index) => (
-          <MealSection 
+          <MealSection
             key={`${section.date}-${index}`}
             date={section.date}
             meals={section.meals}
           />
         ))}
-  
-    </Container>
-        </ScrollView>
+      </Container>
+    </ScrollView>
   );
 }
